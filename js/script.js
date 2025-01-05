b78cc7429a23bacc1ce5ca1ee76630134b3adafd
Jacek Duda <jacek.duda@onet.pl>
2025-01-05 22:09:35 +0100

    initial-base css file separated

--- js/script.js
+++ js/script.js
@@ -1,10 +1,1 @@
 console.log("Hello advanced visitors!");
-
-let button = document.querySelector(".button");
-let header = document.querySelector(".header");
-
-button.addEventListener("click", () => {
-    header.remove();
-});
-
-console.log(button);
